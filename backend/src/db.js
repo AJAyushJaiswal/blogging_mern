85dce3b87fe9560586a7d7c0d695b0da0041fd18
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-10-30 11:00:49 +0530

    remove unnecessary console log

--- backend/src/db.js
+++ backend/src/db.js
@@ -5,7 +5,6 @@ const connectToDB = async () => {
     try{
         const result = await mongoose.connect(`${process.env.DB_CONNECTION_STRING}/${DB_NAME}`);
         if (process.env.NODE_ENV !== 'production') console.log(result.connection.host);
-        console.log(process.env.NODE_ENV);
     }
     catch(error){
         console.log('Error connecting to the database!');
