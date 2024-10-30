77e990e2938a1c2665f437463efdbd302d24a138
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-10-30 12:35:24 +0530

    fix bug in importing error class in global error handler middleware

--- backend/src/middlewares/globalErrorHandler.middleware.js
+++ backend/src/middlewares/globalErrorHandler.middleware.js
@@ -1,4 +1,4 @@
-import {ApiError} from "../utils/ApiError";
+import {ApiError} from "../utils/ApiError.js";
 
 const globalErrorHandler = (err, req, res, next) => {
     if (process.env.NODE_ENV !== 'production') console.log(err);
