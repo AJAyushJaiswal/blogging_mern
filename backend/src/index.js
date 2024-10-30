7d240db46b2c07f5af60471987c25b8bf6fe4adf
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-10-30 11:51:08 +0530

    add code to handle any unhandled errors, network errors or error signals in the application

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,11 @@ import {app} from './app.js';
 
 connectToDB().then(() => {
     try{
+        app.on('error', (error) => {
+            if(process.env.NODE_ENV !== 'production') console.log(`ERROR: ${error}`);
+            process.exit(1);
+        });
+
         const port = process.env.PORT || 7000;
         app.listen(port, () => {
             console.log(`Server started at PORT: ${port}!!`);
