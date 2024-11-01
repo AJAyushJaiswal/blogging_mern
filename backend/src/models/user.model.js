fefaf847c888c42ba410fee1e5c5de50a19de412
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-11-01 21:16:07 +0530

    add pre hook to user schema for hashing password and instance methods to verify password, generate access token and generate refresh token

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,6 @@
 import {Schema, model} from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 
 const userSchema = new Schema({
@@ -18,6 +20,43 @@ const userSchema = new Schema({
         trim: true
     }
 }, {timestamps: true});
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')) return;
+    
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = async function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    ); 
+}
+
+userSchema.methods.generateRefreshToken = async function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+};
 
 
 export const User = model('User', userSchema);
