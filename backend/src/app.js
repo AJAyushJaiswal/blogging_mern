b66301e67822c4abaf2d26692f47d60e9b5f662e
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-11-02 12:08:13 +0530

    create custom router for user and add endpoint to register user

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,6 +18,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 
+// routes
+import userRouter from '../src/routes/user.routes.js';
+
+app.use('/api/v1/users', userRouter);
+
+
 app.use(globalErrorHandler);
 
 export {app};
