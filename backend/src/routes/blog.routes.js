7e2abaaf7e918bc7f755116294a157cdefe49dab
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-11-04 19:03:56 +0530

    setup endpoint to update blog

--- backend/src/routes/blog.routes.js
+++ backend/src/routes/blog.routes.js
@@ -3,7 +3,7 @@ import {verifyAccessToken} from '../middlewares/auth.middleware.js';
 import {upload} from '../middlewares/multer.middleware.js';
 import {imageTooLargeErrorHandler} from '../middlewares/imageTooLargeErrorHandler.middleware.js';
 import {body} from 'express-validator';
-import {publishBlog} from '../controllers/blog.controllers.js';
+import {publishBlog, updateBlog} from '../controllers/blog.controllers.js';
 
 
 const router = Router();
@@ -30,5 +30,27 @@ router.route('/publish').post(
     publishBlog
 );
 
+router.route('/:blogId')
+.post(
+    verifyAccessToken,
+    upload.single('featuredImageFile'),
+    imageTooLargeErrorHandler,
+    [
+        body('title')
+        .notEmpty().withMessage('Title is required!')
+        .isString().withMessage('Title must be a string!')
+        .isLength({min: 3, max: 50}).withMessage('Title must be at least 10 characters!'),
+
+        body('content')
+        .notEmpty().withMessage('Content is required!')
+        .isString().withMessage('Content must be a string!')
+        .isLength({min: 100, max: 1500}).withMessage('Content must be in the range of 100-1500 characters!'),
+        
+        body('status')
+        .optional()
+        .isIn(['private', 'public']).withMessage('Invalid status!')
+    ],
+    updateBlog
+);
 
 export default router;
