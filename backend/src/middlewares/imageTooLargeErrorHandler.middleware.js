b646da752fff31a280c359c9571ffec1c9b973b6
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-11-04 22:55:28 +0530

    fix the imageTooLargeErrorHandler error middeware to handle errors other than the error thrown by multer when image is larger than the given size

--- backend/src/middlewares/imageTooLargeErrorHandler.middleware.js
+++ backend/src/middlewares/imageTooLargeErrorHandler.middleware.js
@@ -1,15 +1,12 @@
+import {ApiError} from "../utils/ApiError.js";
+
 const imageTooLargeErrorHandler = (err, req, res, next) => {
     if(err.code === 'LIMIT_FILE_SIZE'){
-        return res.status(400).json({
-            statusCode: 400,
-            message: "Image must not be larger than 1 MB!",
-            data: null,
-            errors: [],
-            success: false
-        });
+        throw new ApiError(400, "Image must not be larger than 1 MB!");
     }
-
-    next();
+    else if(err){
+        throw err;
+    }
 }
 
 
